11233911c6feb1e56f72fa8ba3575e6a9e1ef696
sw <sw>
2005-02-24 08:43:30 +0000

    *** empty log message ***

--- classes/All.inc.php
+++ classes/All.inc.php
@@ -4,8 +4,16 @@
   require_once $d.'MySQL.inc.php';
 
   require_once $d.'AbstractClass.inc.php';
+  require_once $d.'AbstractTimestampClass.inc.php';
 
+  require_once $d.'Allianz.inc.php';
+  require_once $d.'Archipel.inc.php';
+  require_once $d.'Bauplan.inc.php';
+  require_once $d.'Flotte.inc.php';
   require_once $d.'Insel.inc.php';
+  require_once $d.'Kartenabschnitt.inc.php';
+  require_once $d.'Lager.inc.php';
+  require_once $d.'Mitteilung.inc.php';
   require_once $d.'Schiff.inc.php';
   require_once $d.'Session.inc.php';
   require_once $d.'Spiel.inc.php';

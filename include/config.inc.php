48fa46ca0e4a11b855f1f68580fc6da5c33782d9
sw <sw>
2005-03-14 19:57:01 +0000

    added .cvsignore

--- include/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?
-  $dbserver    = "localhost";  // Datenbankserver
-  $dbuser      = "web4";       // Datenbankbenutzer
-  $dbpassword  = "sw666#GHf";  // Benutzerpasswort
-  $dbdatabase = "usr_web4_2"; // Datenbank
-?>

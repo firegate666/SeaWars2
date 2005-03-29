93eed26ebee3a9007ed5185f87c6a10351fc4980
sw <sw>
2005-03-29 13:19:05 +0000

    *** empty log message ***

--- classes/HTML.inc.php
+++ classes/HTML.inc.php
@@ -3,5 +3,26 @@ class HTML {
 	function tr($content) {
 		return "<tr>$content</tr>\n";
 	}
+	
+	function td($content) {
+		return "<td>$content</td>\n";
+	}
+	
+	function table($content) {
+		if(!is_array($content)) {
+			return '<table>'.$content.'</table>';
+		} else {
+			$rows = '';
+			foreach($content as $row) {
+				$cells = '';
+				foreach($row as $item) {
+					$cells .= HTML::td($item);
+				}
+				$rows .= HTML::tr($cells); 
+			}
+			return '<table>'.$rows.'</table>';
+			
+		}
+	}
 }
 ?>

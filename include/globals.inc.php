f3ce3c267b574fdc6c766a4599b26759a7458ddc
sw <sw>
2005-06-07 21:05:42 +0000

    *** empty log message ***

--- include/globals.inc.php
+++ include/globals.inc.php
@@ -1,52 +1,53 @@
-<?
-  function randomstring($name_laenge) {
-    $zeichen = "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ0123456789";
-    $name_neu = "";
+<?/** * return random string * @name_laenge		length of string */function randomstring($name_laenge) {
+	$zeichen = "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ0123456789";
+	$name_neu = "";
 
-    mt_srand ((double) microtime() * 1000000);
-    for ($i = 0; $i < $name_laenge; $i++) {
-        $name_neu .= $zeichen{mt_rand (0,strlen($zeichen))};
-    }
-    return $name_neu;
-  }
+	mt_srand((double) microtime() * 1000000);
+	for ($i = 0; $i < $name_laenge; $i ++) {
+		$name_neu .= $zeichen {
+			mt_rand(0, strlen($zeichen))
+		};
+	}
+	return $name_neu;
+}
 
-  function redirect($target){
-	$result = array('content'=>'URL', 'target' => $target);
+/** * create redirect */function redirect($target) {
+	$result = array ('content' => 'URL', 'target' => $target);
 	return $result;
-  }		
+}
 
-  function error($error, $class, $method) {
-    $error = new Error($error,$class,$method);
-    die($error->show());
-  }
-  
-  function print_a($array) {
-  	echo("<pre>\n");
-  	print_r($array);
-  	echo("</pre>\n");
-  }
-  
-  /**
-   * Calcualtes distance between (x1, y1) and (x2, y2)
-   * 
-   * @param $x1 x1
-   * @param $y1 y1
-   * @param $x2 x2
-   * @param $y2 y2
-   * @return distance
-   */
-  function distance($x1, $y1, $x2, $y2) {
-  	$diff_x = pow(x2-x1,2);
-  	$diff_y = pow(y2-y1,2);
-  	$result = sqrt(diff_x + diff_y);
-  	return result;
-  }
-  
-  /**
-   * Gets client IP
-   * @return IP as String
-   */
-  function getClientIP() {
-    return $_SERVER['REMOTE_ADDR'];
-  }
+/** * Create error */function error($error, $class, $method) {
+	$error = new Error($error, $class, $method);
+	die($error->show());
+}
+
+/** * improved print_r */function print_a($array) {
+	echo ("<pre>\n");
+	print_r($array);
+	echo ("</pre>\n");
+}
+
+/**
+ * Calcualtes distance between (x1, y1) and (x2, y2)
+ * 
+ * @param $x1 x1
+ * @param $y1 y1
+ * @param $x2 x2
+ * @param $y2 y2
+ * @return distance
+ */
+function distance($x1, $y1, $x2, $y2) {
+	$diff_x = pow(x2 - x1, 2);
+	$diff_y = pow(y2 - y1, 2);
+	$result = sqrt(diff_x + diff_y);
+	return result;
+}
+
+/**
+ * Gets client IP
+ * @return IP as String
+ */
+function getClientIP() {
+	return $_SERVER['REMOTE_ADDR'];
+}
 ?>

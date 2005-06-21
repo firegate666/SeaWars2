ea181946fa07291f211d78834d18a9444d0e38ec
firegate666 <firegate666>
2005-06-21 18:02:43 +0000

    *** empty log message ***

--- install/sql002.sql
+++ install/sql002.sql
@@ -1,4 +1,1 @@
--- removed
 ALTER TABLE `spieler` DROP `bezahlt_bis`;
-ALTER TABLE `archipel` ADD `groessenklasse` TINYINT DEFAULT '1' NOT NULL ;
-ALTER TABLE `archipel` CHANGE `id` `id` BIGINT( 20 ) NOT NULL AUTO_INCREMENT

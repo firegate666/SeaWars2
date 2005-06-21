9b78574ce27254df6d2450ea3fb5a7d33e2a445c
sw <sw>
2005-06-21 09:08:37 +0000

    *** empty log message ***

--- install/sql002.sql
+++ install/sql002.sql
@@ -1,4 +1,4 @@
 -- removed
 ALTER TABLE `spieler` DROP `bezahlt_bis`;
-ALTER TABLE 'archipel' ADD 'groessenklasse' bigint(20) NOT NULL default '1';
-ALTER TABLE 'archipel' MODIFY auto_increment;
+ALTER TABLE `archipel` ADD `groessenklasse` TINYINT DEFAULT '1' NOT NULL ;
+ALTER TABLE `archipel` CHANGE `id` `id` BIGINT( 20 ) NOT NULL AUTO_INCREMENT

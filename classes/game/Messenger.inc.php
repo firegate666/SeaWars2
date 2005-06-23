337a291ddac019f72c9fcf4d2c05edb3fd0cbf57
tensing <tensing>
2005-06-23 16:59:12 +0000

    Messenger test des basistemplates

--- classes/game/Messenger.inc.php
+++ classes/game/Messenger.inc.php
@@ -1,6 +1,27 @@
 <?
+	$template_classes[] = 'messenger';
+
   class Messenger extends AbstractClass {
   	var $mitteilungen;
   	
+  	/**
+	 * check if method is allowed
+	 * @param	String	$method	method to test
+	 * @return	boolean	true/false
+	 */
+	function acl($method){
+          return Login::isLoggedIn();
+        }
+    
+    /**
+	 * Show Messenger using template messenger/page 
+	 * @param	String[]	$vars	request parameter
+	 */
+    function show(&$vars) {
+    	
+    	return "test";
+		//return $this->getLayout($array, "page", $vars);
+	}
+    
   }
 ?>

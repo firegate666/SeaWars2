f7c765e440cd7c81b5b8dacff8c8da6b868779f1
firegate666 <firegate666>
2005-06-24 08:19:52 +0000

    comments added

--- classes/lib/TSP.inc.php
+++ classes/lib/TSP.inc.php
@@ -1,15 +1,31 @@
 <?php
+/**
+ * This is for solving the Traveling Salesman Problem
+ */
 class TSP {
 
 	var $ids; // array of keys
 	var $costs; // array of costs
 
+	/**
+	 * Public constructor
+	 * 
+	 * @param	int[]	$ids	array of ids from all nodes
+	 */
 	function TSP($ids){
 		foreach($ids as $id) {
 			$this->ids[$id] = $id;
 		}
 	}
 	
+	/**
+	 * Calculate best route for given costs, first node from constructor is
+	 * starting node
+	 * 
+	 * @param	int[][]	&$costs	array of costs array[id1][id2] = distanz, after
+	 * calculation, $costs conatins total cost of evaluation
+	 * @return	int[]	array of ids sorted by best route
+	 */
 	function calculate(&$costs) {
 		$this->costs = $costs;
 		$bag = $this->ids;

49726fdc333f2fc522c04d7141d69dd9e851293c
firegate666 <firegate666>
2005-06-30 06:21:08 +0000

    libs moved to end of file, the use some functions from base

--- classes/All.inc.php
+++ classes/All.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once dirname(__FILE__).'/lib/All.inc.php';
 require_once dirname(__FILE__).'/database/All.inc.php';
 require_once dirname(__FILE__).'/base/All.inc.php';
 	
@@ -12,4 +11,5 @@ if(get_config("game", false)) {
 	require_once dirname(__FILE__).'/game/All.inc.php';
 }
 
+require_once dirname(__FILE__).'/lib/All.inc.php';
 ?>

4e9d99984d507108d0f44bf9ee2b99def1618743
firegate666 <firegate666>
2005-06-30 06:16:50 +0000

    Setting added

--- classes/lib/XML.inc.php
+++ classes/lib/XML.inc.php
@@ -1,4 +1,5 @@
 <?php
+	Setting::set('charset', 'ISO-8859-1', false);
 
 /**
 * generate proper XML output
@@ -8,9 +9,9 @@ class XML {
 
     function &body(&$content, $root=null, $doctype=null, $version=null) {
         $xmldef = "<?xml version=\"1.0\" encoding=\"".
-            Setting::get('charset','').'"?>'."\n";
+            Setting::get('charset','ISO-8859-1').'"?>'."\n";
         if(is_null($root) || ($root === true))
-            $root = 'projex';
+            $root = 'seawars';
         $version_attr = '';
         if (is_null($version))
             $version = "1.0";

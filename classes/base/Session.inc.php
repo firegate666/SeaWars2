6db56949e4d88c4e393347b6c9d4123a47fd6011
sw <sw>
2005-07-06 10:33:40 +0000

    comments added default for get added

--- classes/base/Session.inc.php
+++ classes/base/Session.inc.php
@@ -1,41 +1,67 @@
 <?
-  class Session extends AbstractClass {
-    var $sid;
+class Session extends AbstractClass {
+	var $sid;
 
-    function getCookie($name) {
-      return $_SESSION[$name];
-      //return $_COOKIE[$name];
-    }
-    
-    function unsetCookie($name) {
-    	unset($_SESSION[$name]);
-    }
-    
-    function setCookie($name, $value) {
+	/**
+	* Return value from session, if not set return default
+	*
+	* @param	String	$name	name of value
+	* @param	String	$default	default value
+	* @return	String	value
+	*/
+	function getCookie($name, $default = '') {
+		if (isset ($_SESSION[$name]))
+			return $_SESSION[$name];
+		else
+			return $default;
+	}
+
+	/**
+	* Unset value
+	*
+	* @param	String	$name	name of value
+	*/
+	function unsetCookie($name) {
+		unset ($_SESSION[$name]);
+	}
+
+	/**
+	* set value
+	*
+	* @param	String	$name	name of value
+	* @param	String	$value	value
+	*/
+	function setCookie($name, $value) {
 		$_SESSION[$name] = $value;
-      //setcookie($name, $value, NULL);
-    }
-    function removeCookie($name){
-    	$_SESSION[$name] = '';
-      	//setcookie($name,"",0);
-    }
-    function cleanUpCookies(){
-    	$_SESSION = array();
-        //$_COOKIES = array();
-    }
+	}
+	
+	/**
+	* empty value
+	* 
+	* @param	String	$name	name of value
+	*/
+	function removeCookie($name) {
+		$_SESSION[$name] = '';
+	}
+	
+	/**
+	* delete all values from session
+	*/
+	function cleanUpCookies() {
+		$_SESSION = array ();
+	}
 
+	function Session($userid) {
+		$this->data["uid"] = $userid;
+		$this->data["client_ip"] = getClientIP();
+		$this->data["date"] = date("Y-m-d");
+		$this->data["time"] = date("H:m:s");
+		// in Datenbank speichern und ID übergeben
+		$this->sid = $this->store();
+	}
 
-    function Session($userid) {
-      $this->data["uid"]=$userid;
-      $this->data["client_ip"]=getClientIP();
-      $this->data["date"]=date("Y-m-d");
-      $this->data["time"]=date("H:m:s");
-      // in Datenbank speichern und ID übergeben
-      $this->sid = $this->store();
-    }
-
-    function isRegistered() {
-      return ( ($this->id!=NULL) && ($this->username!=NULL) );
-    }
-  }
+	function isRegistered() {
+		return (($this->id != NULL) && ($this->username != NULL));
+	}
+}
 ?>
